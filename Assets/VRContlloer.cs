1234a8442ca8cf78e9292cfc04a4181938c20fda
長島光太 <se.kokuto@gmail.com>
2018-12-24 22:33:16 +0900

    first

---
+++ Assets/VRContlloer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRContlloer : MonoBehaviour {
+
+    public bllet b;
+
+    void Start ()
+    {
+		
+	}
+
+
+    void Update ()
+    {
+        var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを深く引いた");
+            b.Ban();
+        }
+    }
+}
---
+++ Assets/bllet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bllet : MonoBehaviour {
+
+    public GameObject ballet;
+
+    void Start ()
+    {
+		
+	}
+
+    
+    void Update ()
+    {
+		
+	}
+
+    public void Ban()
+    {
+        GameObject a = Instantiate(ballet);
+        a.AddComponent<Rigidbody>();
+        a.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+    }
+}
